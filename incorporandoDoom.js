505eb2cf0ffe3857951d4dd6b68e980d3976996c
Brayan <brayandiaz162@gmail.com>
2022-06-23 17:57:23 -0500

    agregando eventos

--- incorporandoDoom.js
+++ incorporandoDoom.js
@@ -105,17 +105,12 @@ function ordenar() {
 
 //INCORPORANDO DOOM
 
-
+const contenedor=document.getElementById("contenido_principal");
 
 function crearOpciones(){
 // debugger
 
-const alimentos=[1,2,3,4,5];
-
-
-const contenedor=document.getElementById("contenido_principal")
-
-
+const alimentos=[1,2,3,4,5,6];
 
 for (const opcion of alimentos) {
 
@@ -133,4 +128,72 @@ for (const opcion of alimentos) {
 }
 
 crearOpciones()
+
+//EVENTOS
+
+const usuario=document.getElementById("usuario");
+const clave=document.getElementById("clave");
+
+let correoUsuario=" ";
+
+usuario.addEventListener("blur", (e)=>{
+        
+correoUsuario= e.target.value;
+
+console.log(correoUsuario);
+
+
+});   
+
+//------------------------------------------------------------------------------------
+
+let claveUsuario="";
+
+clave.addEventListener("blur", (e)=>{
+        
+    claveUsuario= e.target.value;
+    
+    console.log(claveUsuario);
+    
+    
+    });   
+
+//-----------------------------------------------------------------------------------------------
+
+function tocarBotones(){
+
+const botones=document.getElementsByClassName("boton");
+
+for (const boton of botones) {
+    
+    boton.addEventListener("mouseover",()=>boton.className="sobreBoton");
+    boton.addEventListener("mouseout",()=>boton.className="");
+
+}
+
+}
+
+tocarBotones();
+
+//-----------------------------------------------------------------------------------------
+
+document.addEventListener("submit", (e)=>{
+
+e.preventDefault(e)
+
+alert("Iniciando sesion")
+
+});
+
+
+//------------------------------------------------------------------------------------
+
+const opciones=document.getElementsByClassName("platillo");
+
+for (const opcion of opciones) {
+
+opcion.addEventListener("click",()=> opcion.style.background="black");
+opcion.addEventListener("dblclick",()=> opcion.style.background="white");
+
+}
 
